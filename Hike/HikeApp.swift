8209ee1368435d6887ac555c968e40961bd0126a
AntonioDamjaanovic <damjanovic1109@gmail.com>
2025-12-23 11:24:47 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Antonio Damjanović on 23.12.2025..
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
